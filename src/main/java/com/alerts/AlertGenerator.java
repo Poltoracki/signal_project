481d5b0d97996259633a928300a2b4220854cfdd
Dimitrov <lolliker12@gmail.com>
2025-04-17 17:40:12 +0200

    Completed alerts for blood pressure

--- src/main/java/com/alerts/AlertGenerator.java
+++ src/main/java/com/alerts/AlertGenerator.java
@@ -33,16 +33,108 @@ public class AlertGenerator {
     /**
      * Evaluates the specified patient's data to determine if any alert conditions are met.
      * If a condition is met, an alert is triggered via the {@link #triggerAlert(Alert)} method.
-     *
+     * If an alert is triggered, it needs to be added to the {@link AlertManager} repository.
+     * 
      * // Improved readability by removing unnecessary line breaks and corrected
      * // JavaDoc inline link formatting to explicitly include the method parameter.
      *
      * @param patient The patient data to evaluate for alert conditions
      */
     public void evaluateData(Patient patient, long startTime, long endTime) {
-        // Implementation goes here
-    }
+        List<PatientRecord> records = dataStorage.getRecords(patient.getId(), startTime, endTime);
+        //sort the records so that they're chronically aligned
+        records.sort((r1, r2) -> Long.compare(r1.getTimestamp(), r2.getTimestamp()));
 
+        // Blood pressure alerts
+        List<PatientRecord> sBloodRecords = dataStorage.getRecords("systolic blood pressure", records);
+        List<PatientRecord> dBloodRecords = dataStorage.getRecords("diastolic blood pressure", records);
+
+        // Systolic blood pressure
+        if(sBloodRecords.size() < 1) {
+            System.out.println("Insufficient number of records to check for systolic blood pressure!");
+        }
+        else {
+            for(int i = 0; i < sBloodRecords.size(); i++)
+            {
+                if( i < sBloodRecords.size()-3)
+                {
+                // Checking if it's decreasing
+                if((sBloodRecords.get(i).getMeasurementValue() - sBloodRecords.get(i+1).getMeasurementValue()) > 10 &&
+                (sBloodRecords.get(i+1).getMeasurementValue() - sBloodRecords.get(i+2).getMeasurementValue()) > 10 &&
+                (sBloodRecords.get(i+2).getMeasurementValue() - sBloodRecords.get(i+3).getMeasurementValue()) > 10)
+                {
+                    Alert alert = new Alert(Integer.toString(patient.getId()), "Decreasing trend in systolic blood pressure", sBloodRecords.get(i+3).getTimestamp());
+                    triggerAlert(alert);
+                }
+
+                // Checking if it's increasing
+                if((sBloodRecords.get(i).getMeasurementValue() - sBloodRecords.get(i+1).getMeasurementValue()) < -10 &&
+                (sBloodRecords.get(i+1).getMeasurementValue() - sBloodRecords.get(i+2).getMeasurementValue()) < -10 &&
+                (sBloodRecords.get(i+2).getMeasurementValue() - sBloodRecords.get(i+3).getMeasurementValue()) < -10)
+                {
+                    Alert alert = new Alert(Integer.toString(patient.getId()), "Increasing trend in systolic blood pressure", sBloodRecords.get(i+3).getTimestamp());
+                    triggerAlert(alert);
+                }
+                }
+
+                // Checking for critical upper threshold
+                if(sBloodRecords.get(i).getMeasurementValue() > 180) {
+                    Alert alert = new Alert(Integer.toString(patient.getId()), "Passed critial upper threshold for systolic blood pressure", sBloodRecords.get(i).getTimestamp());
+                    triggerAlert(alert);
+                }
+
+                // Checking for critical lower threshold
+                if(sBloodRecords.get(i).getMeasurementValue() < 90) {
+                    Alert alert = new Alert(Integer.toString(patient.getId()), "Passed critial lower threshold for systolic blood pressure", sBloodRecords.get(i).getTimestamp());
+                    triggerAlert(alert);
+                }
+            }
+        }
+
+        // Diastolic blood pressure
+        if(dBloodRecords.size() < 1) {
+            System.out.println("Insufficient number of records to check for diastolic blood pressure!");
+        }
+        else {
+            for(int i = 0; i < dBloodRecords.size(); i++)
+            {
+                if(i < dBloodRecords.size()-3)
+                {
+                // Checking if it's decreasing
+                if((dBloodRecords.get(i).getMeasurementValue() - dBloodRecords.get(i+1).getMeasurementValue()) > 10 &&
+                (dBloodRecords.get(i+1).getMeasurementValue() - dBloodRecords.get(i+2).getMeasurementValue()) > 10 &&
+                (dBloodRecords.get(i+2).getMeasurementValue() - dBloodRecords.get(i+3).getMeasurementValue()) > 10)
+                {
+                    Alert alert = new Alert(Integer.toString(patient.getId()), "Decreasing trend in diastolic blood pressure", dBloodRecords.get(i+3).getTimestamp());
+                    triggerAlert(alert);
+                }
+
+                // Checking if it's increasing
+                if((dBloodRecords.get(i).getMeasurementValue() - dBloodRecords.get(i+1).getMeasurementValue()) < -10 &&
+                (dBloodRecords.get(i+1).getMeasurementValue() - dBloodRecords.get(i+2).getMeasurementValue()) < -10 &&
+                (dBloodRecords.get(i+2).getMeasurementValue() - dBloodRecords.get(i+3).getMeasurementValue()) < -10)
+                {
+                    Alert alert = new Alert(Integer.toString(patient.getId()), "Increasing trend in diastolic blood pressure", dBloodRecords.get(i+3).getTimestamp());
+                    triggerAlert(alert);
+                }
+                }
+
+                // Checking for critical upper threshold
+                if(dBloodRecords.get(i).getMeasurementValue() > 120) {
+                    Alert alert = new Alert(Integer.toString(patient.getId()), "Passed critial upper threshold for systolic blood pressure", dBloodRecords.get(i).getTimestamp());
+                    triggerAlert(alert);
+                }
+
+                // Checking for critical lower threshold
+                if(dBloodRecords.get(i).getMeasurementValue() < 60) {
+                    Alert alert = new Alert(Integer.toString(patient.getId()), "Passed critial lower threshold for systolic blood pressure", dBloodRecords.get(i).getTimestamp());
+                    triggerAlert(alert);
+                }
+            }
+        }
+
+
+    }
 
     /**
      * Triggers an alert for the monitoring system. This method can be extended to
@@ -55,5 +147,6 @@ public class AlertGenerator {
      */
     private void triggerAlert(Alert alert) {
         System.out.println("Patient: " + alert.getPatientId() + "; Condition: " + alert.getCondition() + "; Time: " + alert.getTimestamp() + ";");
+        // AlertManager.add(alert);
     }
 }
