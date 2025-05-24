23a5bf15ab1853f77c11fbdbcc72257e5d52b001
Dimitrov <lolliker12@gmail.com>
2025-05-24 16:01:02 +0200

    Modified DataReader to work with WebSocket

--- src/main/java/com/data_management/DataReader.java
+++ src/main/java/com/data_management/DataReader.java
@@ -1,6 +1,7 @@
 package com.data_management;
 
 import java.io.IOException;
+import java.net.http.WebSocket;
 
 public interface DataReader {
     /**
@@ -9,5 +10,5 @@ public interface DataReader {
      * @param dataStorage the storage where data will be stored
      * @throws IOException if there is an error reading the data
      */
-    void readData(DataStorage dataStorage) throws IOException;
+    void readData(WebSocket socket) throws IOException;
 }
